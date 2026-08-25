8bbcd96ea7f35e7a8005456ae274c6e5d9db287b
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-25 17:21:02 +0200

    feat(rate-limiter): Add a global rate limiter

---
+++ CodeWithMixx.API/Infrastructure/RateLimiting/GlobalRateLimiter.cs
@@ -0,0 +1,42 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace CodeWithMixx.API.Infrastructure.RateLimiting;
+
+public static class GlobalRateLimiter
+{
+    public static void AddGlobalRateLimiter(this IServiceCollection services)
+    {
+        services.AddRateLimiter(options =>
+        {
+            options.OnRejected = async (context, cancellationToken) =>
+            {
+                var problem = new ProblemDetails
+                {
+                    Title = "Too many requests",
+                    Type = "urn:code-with-mixx-api:error:General.TooManyRequests",
+                    Detail = "You have exceeded the allowed number of requests. Please try again later.",
+                    Status = StatusCodes.Status429TooManyRequests,
+                    Instance = context.HttpContext.Request.Path,
+                    Extensions = { ["errorCode"] = "General.TooManyRequests" }
+                };
+
+                await context.HttpContext.Response.WriteAsJsonAsync(problem, cancellationToken);
+            };
+            
+            options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+            {
+                var partitionKey = context.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP";
+
+                return RateLimitPartition.GetTokenBucketLimiter(partitionKey, _ => new TokenBucketRateLimiterOptions
+                {
+                    TokenLimit = 10,
+                    ReplenishmentPeriod = TimeSpan.FromSeconds(15),
+                    TokensPerPeriod = 10
+                });
+
+            });
+        });
+    }
+}
--- CodeWithMixx.API/Program.cs
+++ CodeWithMixx.API/Program.cs
@@ -14,6 +14,7 @@ var app = builder.Build();
 await app.MapSeeders();
 app.MapEndpoints();
 
+app.UseRateLimiter();
 
 if (app.Environment.IsDevelopment())
 {
@@ -35,7 +36,6 @@ app.UseSerilogRequestLogging(options =>
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.UseHttpsRedirection();
 
 app.Run();
