97b6db74ec976fb45cc475250b0c7333cce3e119
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-02 20:36:35 +0200

    wip(reservations): Create domain model reservations

---
+++ CodeWithMixx.API/Domain/Entities/Reservations/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using CodeWithMixx.API.Domain.Entities.Admins;
+using CodeWithMixx.API.Domain.Entities.Students;
+
+namespace CodeWithMixx.API.Domain.Entities.Reservations
+{
+    public class Reservation : IAuditable
+    {
+        public int Id { get; set; }
+        public ReservationStatus ReservationStatus { get; set; } = ReservationStatus.Confirmed;
+        public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Pending;
+
+        public decimal TotalPrice { get; set; }
+        public decimal DiscountRate { get; set; }
+        public decimal Bonus { get; set; }
+
+        public string? Notes { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public Admin Admin { get; set; } = null!;
+        public string AdminId { get; set; } = null!;
+
+        public Student Student { get; set; } = null!;
+        public string StudentId { get; set; } = null!;
+
+
+    }
+}
---
+++ CodeWithMixx.API/Domain/Entities/Reservations/ReservationErrors.cs
@@ -0,0 +1,28 @@
+﻿using CodeWithMixx.API.Common.Result;
+
+namespace CodeWithMixx.API.Domain.Entities.Reservations
+{
+    public static class ReservationError
+    {
+        public static Error NotFound(int? identifier = null)
+        {
+            string message = identifier is null
+                ? "Reservation not found"
+                : $"Reservation with identifier '{identifier}' is not found";
+
+            return new Error("Reservation.NotFound", message, ErrorType.NotFound);
+        }
+
+        public static Error AlreadyPaid(int? identifier = null)
+        {
+            string message = identifier is null
+                ? "Reservation is already fully paid"
+                : $"Reservation with identifier '{identifier}' is already fully paid";
+
+            return new Error("Reservation.AlreadyPaid", message, ErrorType.Validation);
+        }
+
+        public static Error InvalidAmount(decimal amount)
+            => new("Reservation.InvalidAmount", $"Amount '{amount}' must be greater than zero", ErrorType.Validation);
+    }
+}
