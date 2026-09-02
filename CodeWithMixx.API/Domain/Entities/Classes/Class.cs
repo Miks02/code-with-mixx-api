41452b04ef4b3e8413ed8a40c682c01e36410f46
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-02 20:36:56 +0200

    wip(classes): Create domain model Class

---
+++ CodeWithMixx.API/Domain/Entities/Classes/Class.cs
@@ -0,0 +1,18 @@
+﻿using CodeWithMixx.API.Domain.Entities.Reservations;
+using CodeWithMixx.API.Domain.Entities.Subjects;
+
+namespace CodeWithMixx.API.Domain.Entities.Classes
+{
+    public class Class 
+    {
+        public int Id { get; set; }
+        public DateTime StartsAt { get; set; }
+        public DateTime EndsAt { get; set; }
+
+        public Reservation Reservation { get; set; } = null!;
+        public int ReservationId { get; set; } 
+        public Subject Subject { get; set; } = null!;
+        public int SubjectId { get; set; }
+
+    }
+}
---
+++ CodeWithMixx.API/Domain/Entities/Classes/ClassErrors.cs
@@ -0,0 +1,19 @@
+﻿using CodeWithMixx.API.Common.Result;
+
+namespace CodeWithMixx.API.Domain.Entities.Classes
+{
+    public static class ClassError
+    {
+        public static Error NotFound(int? identifier = null)
+        {
+            string message = identifier is null
+                ? "Class not found"
+                : $"Class with identifier '{identifier}' is not found";
+
+            return new Error("Class.NotFound", message, ErrorType.NotFound);
+        }
+
+        public static Error InvalidSchedule(DateTime startsAt, DateTime endsAt)
+            => new("Class.InvalidSchedule", $"Class schedule is invalid: starts at '{startsAt:u}' and ends at '{endsAt:u}'", ErrorType.Validation);
+    }
+}
