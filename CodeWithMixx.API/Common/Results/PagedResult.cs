8483af7de86524e7a34a9d2dc84c95cbd8d641a2
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-20 14:53:03 +0200

    feat(pagination): Make pageSize and pageNumber nullable in PagedResult<T>

--- CodeWithMixx.API/Common/Results/PagedResult.cs
+++ CodeWithMixx.API/Common/Results/PagedResult.cs
@@ -4,31 +4,35 @@ namespace CodeWithMixx.API.Common.Results;
 
 public class PagedResult<T>(IReadOnlyList<T> items, int pageNumber, int pageSize, int totalCount)
 {
-    public IReadOnlyList<T> Items { get; init; } = items;
-    public int PageNumber { get; init; } = pageNumber;
-    public int PageSize { get; init; } = pageSize;
+    public IReadOnlyList<T> Items { get; } = items;
+    public int PageNumber { get; } = pageNumber;
+    public int PageSize { get; } = pageSize;
     public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     public bool HasPreviousPage => PageNumber > 1;
     public bool HasNextPage => PageNumber < TotalPages;
     public int PaginatedCount => Items.Count;
-    public int TotalCount { get; init; } = totalCount;
-
-    public static async Task<PagedResult<T>> CreateAsync(IQueryable<T> query, int pageNumber, int pageSize,
+    public int TotalCount { get; } = totalCount;
+    
+    public static async Task<PagedResult<T>> CreateAsync(IQueryable<T> query, int? pageNumber, int? pageSize,
         CancellationToken ct = default)
     {
-        if (pageSize > 100)
-            pageSize = 100;
+        if(!pageNumber.HasValue || pageNumber.Value < 1)
+            pageNumber = 1;
         
-        if(pageSize < 1)
+        if(!pageSize.HasValue || pageSize.Value < 1)
             pageSize = 1;
         
-        if(pageNumber < 1)
-            pageNumber = 1;
+        if (pageSize > 100)
+            pageSize = 100;
 
         var totalCount = await query.CountAsync(ct);
-        return new PagedResult<T>(await query.Skip((pageNumber - 1) * pageSize)
-            .Take(pageSize)
-            .ToListAsync(ct), pageNumber, pageSize, totalCount);
+        
+        if(totalCount == 0)
+            return new PagedResult<T>([], pageNumber.Value, pageSize.Value, totalCount);
+        
+        return new PagedResult<T>(await query.Skip((pageNumber.Value - 1) * pageSize.Value)
+            .Take(pageSize.Value)
+            .ToListAsync(ct), pageNumber.Value, pageSize.Value, totalCount);
     }
 
 }
