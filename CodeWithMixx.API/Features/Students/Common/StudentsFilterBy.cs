42b8d9a144ffe025acb0310c7811850dfb21fe17
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-20 14:53:17 +0200

    feat(students): Add enums for sorting and filtering students

---
+++ CodeWithMixx.API/Features/Students/Common/StudentsFilterBy.cs
@@ -0,0 +1,9 @@
+namespace CodeWithMixx.API.Features.Students.Common;
+
+public enum StudentsFilterBy
+{
+    WithClasses,
+    WithoutClasses,
+    WithProjects,
+    WithoutProjects,
+}
---
+++ CodeWithMixx.API/Features/Students/Common/StudentsSortBy.cs
@@ -0,0 +1,11 @@
+namespace CodeWithMixx.API.Features.Students.Common;
+
+public enum StudentsSortBy
+{
+    CreatedAtAscending,
+    CreatedAtDescending,
+    StudentNameAscending,
+    StudentNameDescending,
+    TotalReservationAscending,
+    TotalReservationDescending,
+}
