5e2f28244b3eeb76977555e6d02189ff8b78c2ce
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-21 23:29:18 +0200

    feat(validation): Add StudentsFilterResolver static class for validating a list of filters that come by as a request from the client

---
+++ CodeWithMixx.API/Features/Students/Common/StudentsFilterResolver.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeWithMixx.API.Features.Students.Common
+{
+    public static class StudentsFilterResolver
+    {
+        private static readonly Dictionary<StudentsFilterBy, List<StudentsFilterBy>> ConflictingFilters = new()
+            {
+                [StudentsFilterBy.WithClasses] = [StudentsFilterBy.WithoutClasses],
+                [StudentsFilterBy.WithoutClasses] = [StudentsFilterBy.WithClasses],
+                [StudentsFilterBy.WithProjects] = [StudentsFilterBy.WithoutProjects],
+                [StudentsFilterBy.WithoutProjects] = [StudentsFilterBy.WithProjects],
+            };
+
+        public static IReadOnlyList<StudentsFilterBy> GetInvalidFilters(IReadOnlyList<StudentsFilterBy> selectedFilters)
+        {
+            List<StudentsFilterBy> invalidFilters = [];
+
+            foreach (var filter in selectedFilters)
+            {
+                if (invalidFilters.Contains(filter))
+                    continue;
+
+                if (ConflictingFilters.TryGetValue(filter, out var conflicts))
+                {
+                    invalidFilters.AddRange(conflicts);
+                }
+            }
+
+            return invalidFilters;
+        }
+    }
+}
